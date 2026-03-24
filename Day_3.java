a925a754fd36af8befc1050f277a34f3160222d4
Charliee311 <codewithkushwaha@gmail.com>
2026-03-24 17:44:01 +0530

    Day_3 Modified

--- Day_3.java
+++ Day_3.java
@@ -3,56 +3,55 @@ package Ques_Practise.Basics.Day_02;
 import java.util.Arrays;
 
 public class Day_3 {
-    static int[] nat_num(int n){
-        int[] arr = new int[n];
+    // PRINT 1->N
+    public static void print1(int n){
         int i = 1;
         while(i<=n){
-            arr[i-1]=i;
+            System.out.println(i);
             i++;
         }
-        return arr;
     }
 
-    static int[] nat_num2(int n){
-        int[] arr = new int[n];
-        for (int i = n; i > 0; i--) {
-            arr[n-i]=i;
+    // PRINT N->1
+    public static void print2(int n){
+        int i = n;
+        while(i>0){
+            System.out.println(i);
+            i--;
         }
-        return arr;
     }
-    static int[] even_num(int n){
-        int i = 2;
-        int j = 0;
-        int[] arr = new int[n];
 
-        while (j < n) {
-            arr[j] = i;
-            j++;
-            i += 2;
+    // PRINT EVEN TILL N
+    public static void print3(int n){
+        for (int i = 1; i <=n; i++) {
+            if (i%2==0){
+                System.out.println(i);
+            }
         }
-        return arr;
     }
-    static int[] odd_num(int n){
-        int i = 1;
-        int j = 0;
-        int[] arr = new int[n];
 
-        while (j < n) {
-            arr[j] = i;
-            j++;
-            i += 2;
+    // PRINT ODD TILL N
+    public static void print4(int n){
+        for (int i = 1; i <=n; i++) {
+            if (i%2!=0){
+                System.out.println(i);
+            }
         }
-        return arr;
     }
-    static int power(int a,int b){
-        int value = 1;
-        for(int i=1;i<=b;i++){
-            value=a*value;
+
+    // Power a^b without Math.pow
+    public static int power(int n,int pow){
+        int total = 1;
+        for (int i = 1; i <= pow; i++) {
+            total=total*n;
         }
-        return value;
+        return total;
     }
+
     public static void main(String[] args) {
-
-
+        int result = power(3,3);
+        System.out.println(result);
     }
+
+
 }
