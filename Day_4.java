4a8fd4c6b44b9330a3e02a80c2e0f911419ec129
Charliee311 <codewithkushwaha@gmail.com>
2026-03-24 20:17:17 +0530

    Day-4 Modified

--- Day_4.java
+++ Day_4.java
@@ -1,49 +1,40 @@
 
 package Ques_Practise.Basics.Day_02;
 
-
-
 public class Day_4 {
-    static String prime_num(int n){
+    public static int fact(int n){
+        int fact = 1;
+        for (int i = 1; i <=n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static String primeNum(int n){
         for (int i = 2; i <=n/2; i++) {
-            if(n%i==0){
-                return "not prime";
+            if (n%i==0){
+                return "not Prime";
             }
         }
         return "prime";
     }
-    static int factorial(int n){
-        if (n==0){
-            return 0;
-        }
-        int i =1;
-        int p = 0;
-        int count=2;
 
-        while(count<=n){
-            int temp=i;
-            i=i+p;
-            p=temp;
-            count++;
-        }
-        return i;
-    }
-
-    static void printPrimes(int n){
-        for (int i = 2; i <=n; i++) {
+    public static void allPrime(int n){
+        for (int i = 2; i <= n; i++) {
             boolean isPrime = true;
-            for (int j = 2; j <=Math.sqrt(i) ; j++) {
+            for (int j = 2; j <= i/2 ; j++) {
                 if (i%j==0){
-                    isPrime=false;
+                    isPrime = false;
                     break;
                 }
             }
-            if (isPrime==true){
+            if (isPrime){
                 System.out.println(i);
             }
         }
     }
     public static void main(String[] args) {
-        printPrimes(10);
+        allPrime(11);
     }
 }
+
