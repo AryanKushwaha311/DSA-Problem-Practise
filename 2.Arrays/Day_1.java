370ddd0d40f6efdcddd14fcf9bb380ab86a3a0c2
Charliee311 <codewithkushwaha@gmail.com>
2026-03-29 10:39:03 +0530

    Question Correction

--- 2.Arrays/Day_1.java
+++ 2.Arrays/Day_1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Day_1{
     
     // Question-1
@@ -70,12 +72,15 @@ public class Day_1{
 
     // Question-8
     public static int secondLargest(int[] arr){
-        int max1 = arr[0];
-        int max2 = arr[1];
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
 
-        for (int i = 0; i < arr.length-1; i++) {
-            if (arr[i+1] > arr[i]){
-                max1 = arr[i+1];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max1) {
+                max2 = max1;
+                max1 = arr[i];
+            }
+            else if (arr[i] > max2 && arr[i] != max1) {
                 max2 = arr[i];
             }
         }
@@ -105,14 +110,37 @@ public class Day_1{
     // Question - 11
     public static String checkSoretedArray(int[] arr){
         for (int i = 0; i < arr.length-1; i++) {
-            if (arr[i+1] < arr[i]){
+            if (arr[i] > arr[i+1]){
                 return "Not Sorted";
             }
         }
         return "Sorted";
     }
 
+    // Question - 12
+    public static void allZeroAtEnd(int[] arr){
+        int[] newArr = new int[arr.length];
+        int j = 0;
+
+        for (int i = 0; i<arr.length; i++){
+            if (arr[i]!=0){
+                newArr[j] = arr[i];
+                j++;
+            }
+        }
+
+        for (int i = 0; i<arr.length; i++){
+            if (arr[i] == 0){
+                newArr[j] = arr[i];
+                j++;
+            }
+        }
+
+        System.out.println(Arrays.toString(newArr));
+    }
+
     public static void main(String[] args) {
-        
+        int result = secondLargest(new int[]{10, 5, 8});
+        System.out.println(result);
     }
 }
---
+++ 2.Arrays/Day_2.java
@@ -0,0 +1,11 @@
+import javax.security.sasl.SaslClient;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day_2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("size of row: ");
+
+    }
+}
